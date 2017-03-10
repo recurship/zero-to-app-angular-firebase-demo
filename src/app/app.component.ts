77dfc9e2428a83ff628cf5b8819b24dcdeb96348
Mashhood Rastgar <mashhoodr@gmail.com>
2017-03-10 11:45:20 +0500

    added firebase stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable, FirebaseAuthState } from 'angularfire2';
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
@@ -8,4 +9,62 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  zweets: FirebaseListObservable<any[]>;
+  user: FirebaseAuthState;
+  constructor(private af: AngularFire) {
+    this.af.auth.subscribe(auth => {
+      if (auth && auth.auth) {
+        console.log('User', auth, auth.auth.email);
+        this.user = auth;
+      } else {
+        console.log('No user available.');
+      }
+    });
+
+    this.zweets = this.af.database.list('/');
+  }
+
+  add(field) {
+    this.zweets.push({
+      text: field.value,
+      createdAt: (new Date()).toString(),
+      user: this.user.auth.email,
+      likes: 0
+    });
+
+    field.value = '';
+  }
+
+  like(uid: string, zweet) {
+    if (!zweet.likes) { zweet.likes = 0; }
+    this.zweets.update(uid, { likes: zweet.likes + 1 })
+  }
+
+  delete(uid: string) {
+    this.zweets.remove(uid);
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
+
+  login(form) {
+    this.af.auth.login({
+      email: form.value.email,
+      password: form.value.password
+    },
+    {
+      provider: AuthProviders.Password,
+      method: AuthMethods.Password,
+    });
+    form.reset();
+  }
+
+  signup(form) {
+    this.af.auth.createUser({
+      email: form.value.email,
+      password: form.value.password
+    });
+    form.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,16 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDrlShh3TLtYTPOf2DNzVVMP5IZD2hoG4w',
+  authDomain: 'zwitter-53bc9.firebaseapp.com',
+  databaseURL: 'https://zwitter-53bc9.firebaseio.com',
+  storageBucket: 'zwitter-53bc9.appspot.com',
+  messagingSenderId: '342030132721'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +24,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
