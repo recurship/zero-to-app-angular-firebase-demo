81c3586c7b202155dd78364cb2b909d89e72d39b
Mashhood Rastgar <mashhoodr@gmail.com>
2017-03-10 09:46:38 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app works!';
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
