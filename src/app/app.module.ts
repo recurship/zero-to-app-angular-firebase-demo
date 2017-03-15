f6a16328cb83b3adb97dbcc0cef7d4a293169b1d
Mashhood Rastgar <mashhoodr@gmail.com>
2017-03-15 10:30:31 +0500

    reset modules and package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
 
-// Must export the config
-export const firebaseConfig = {
-  apiKey: 'AIzaSyDrlShh3TLtYTPOf2DNzVVMP5IZD2hoG4w',
-  authDomain: 'zwitter-53bc9.firebaseapp.com',
-  databaseURL: 'https://zwitter-53bc9.firebaseio.com',
-  storageBucket: 'zwitter-53bc9.appspot.com',
-  messagingSenderId: '342030132721'
-};
 
 @NgModule({
   declarations: [
@@ -24,8 +15,7 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
